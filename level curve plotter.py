fac9cdef51d4420486790ec15d3fa9e86418f17b
Mongolito <sebastiencoube@gmail.com>
2018-01-30 11:34:28 +0100

    Create level curve plotter.py

---
+++ level curve plotter.py
@@ -0,0 +1,22 @@
+import matplotlib
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+def bivariate_plotter (x_min, x_max, y_min, y_max, delta, f, title = "") : 
+    x = np.arange(x_min, x_max, delta)
+    y = np.arange(y_min, y_max, delta)
+    X, Y = np.meshgrid(x, y)
+    Z =f(X, Y)
+    plt.figure()
+    CS = plt.contour(X, Y, Z)
+    plt.clabel(CS, inline=1, fontsize=10)
+    plt.title(title)
+def f(x, y) : 
+    return(mlab.bivariate_normal(x, y, 1.0, 1.0, 3,3))
+bivariate_plotter(-5, 5, -5, 5, 0.1, f)
+
+def f(x, y) : 
+    return(x*y)
+bivariate_plotter(-5, 5, -5, 5, 0.1, f)
